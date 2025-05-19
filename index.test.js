caf7a9837c87494c9a22e872c7d04fa5178dafb7
Lauren Balch <llbalch@gmail.com>
2025-05-19 13:48:01 -0500

    added route for musicians and bands along with tests

--- index.test.js
+++ index.test.js
@@ -1,22 +1,36 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
-const {seedMusician} = require("./seedData");
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
+const { seedMusician } = require("./seedData");
 
+describe("./musicians endpoint", () => {
+  // testing musicians endpoint
+  test("testing musicians endpoint", async () => {
+    // send a request to '/musicians' endpoint
+    const responseMusician = await request(app).get("/musicians");
+    // to convert the JSON string back to JSON object where we can access values, use JSON.parse()
+    const responseData = JSON.parse(responseMusician.text);
+    // 200 indicates a successful GET request was made
+    expect(responseMusician.statusCode).toBe(200);
+    expect(responseData[2].instrument).toEqual("Guitar");
+  });
+});
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+describe("./bands endpoint", () => {
+  // testing bands endpoint
+  test("testing bands endpoint", async () => {
+    // send a request to '/bands' endpoint
+    const responseBand = await request(app).get("/bands");
+    // to convert the JSON string back to JSON object where we can access values, use JSON.parse()
+    const responseData2 = JSON.parse(responseBand.text);
+    // 200 indicates a successful GET request was made
+    expect(responseBand.statusCode).toBe(200);
+    expect(responseData2);
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require("../models/index");
+const { db } = require("../db/connection");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
+//TODO: Create a GET /musicians route to return all musicians
+app.get("/musicians", async (req, res) => {
+  const musicians = await Musician.findAll({});
+  // send books converted as a json string
+  res.json(musicians);
+});
 
+app.get("/musicians/1", async (req, res) => {
+  const musician1 = await Musician.findByPk(1);
+  res.json(musician1);
+});
 
+app.get("/musicians/2", async (req, res) => {
+  const musician2 = await Musician.findByPk(2);
+  res.json(musician2);
+});
 
+app.get("/musicians/3", async (req, res) => {
+  const musician3 = await Musician.findByPk(3);
+  res.json(musician3);
+});
 
-
-
+app.get("/bands", async (req, res) => {
+  const bands = await Band.findAll({});
+  res.json(bands);
+});
 
 module.exports = app;
